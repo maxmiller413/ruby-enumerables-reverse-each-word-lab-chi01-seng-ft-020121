a5c6c0ea2a3d9f26268e94295df984e5b5669506
Max Miller <maxmiller413@gmail.com>
2021-01-31 04:41:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,3 @@
 def reverse_each_word(sentence)
-  words = sentence.split(" ")
-  reverse_word = []
-    words.collect do |element|
-      reverse_word << element.reverse 
-  end 
-  reverse_word.join(" ")
+  
 end
